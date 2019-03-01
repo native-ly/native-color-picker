ad24a4546fece7e6e9b31f4471664a934638c9a0
Jakub Biesiada <biesiadajakub@icloud.com>
2019-03-02 00:32:48 +0100

    Update Props.tsx

--- src/interfaces/Props.tsx
+++ src/interfaces/Props.tsx
@@ -1,10 +1,10 @@
 export default interface Props {
-  colors?: string[]
-  selectedColor?: string
-  onSelect: (item: string) => void
-  horizontal?: boolean
-  applyGradient?: boolean
-  columns?: number
-  itemSize?: number
-  marker?: 'border' | 'checkmark' | 'fade' | 'none'
+  readonly colors?: string[]
+  readonly selectedColor?: string
+  readonly onSelect?: (item: string) => void
+  readonly horizontal?: boolean
+  readonly applyGradient?: boolean
+  readonly columns?: number
+  readonly itemSize?: number
+  readonly marker?: 'border' | 'checkmark' | 'fade' | 'none'
 }
