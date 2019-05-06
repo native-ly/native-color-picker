ae0b575aa5fd7532c23e73505090c7f595f46b06
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-06 18:34:02 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ module.exports = env => {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: ['ts-loader'],
-          exclude: [/node_modules/],
+          loader: 'ts-loader',
+          exclude: /node_modules/,
           resolve: {
             extensions: ['.tsx'],
           },
