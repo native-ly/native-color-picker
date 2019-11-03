6b0eeedd5c77c2df87fe3eb98d164ff7d6fc47ac
Jakub Biesiada <biesiadajakub@icloud.com>
2019-11-03 01:46:55 +0100

    Update types

--- src/interfaces/Marker.tsx
+++ src/interfaces/Marker.tsx
@@ -1,11 +1,11 @@
 import { ViewProps } from 'react-native'
 
-import { Animate, MarkerType } from '../types'
+import { Animate, MarkerDisplay, MarkerType } from '../types'
 
 export interface Marker extends ViewProps {
   readonly animate?: Animate
   readonly color: string
   readonly size: number
-  readonly display: 'adjust' | 'contrast' | string
+  readonly display: MarkerDisplay
   readonly type: MarkerType
 }
