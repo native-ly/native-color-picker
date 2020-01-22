b228ab124063e340975252371f7e2e4aeded88c0
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-22 15:22:39 +0100

    Fix

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
