7018cb45d44e3109a96f8047b8087d9c8edea9ff
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-17 18:43:05 +0200

    chore: exclude lib in .gitignore

--- lib/interfaces/Marker.d.ts
+++
@@ -1,9 +0,0 @@
-import { ViewProps } from 'react-native';
-import { Animate, MarkerDisplay, MarkerType } from '../types';
-export interface Marker extends ViewProps {
-    readonly animate?: Animate;
-    readonly color: string;
-    readonly size: number;
-    readonly display: MarkerDisplay;
-    readonly type: MarkerType;
-}
--- lib/interfaces/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Gradient as GradientProps } from './Gradient';
-export { Item as ItemProps } from './Item';
-export { Marker as MarkerProps } from './Marker';
-export { Props } from './Props';
