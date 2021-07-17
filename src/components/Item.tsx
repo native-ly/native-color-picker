2a00656dccce1212c2e521d57234b9b0aee976b9
Jakub Biesiada <biesiadajakub@icloud.com>
2021-07-17 17:48:23 +0200

    refactor: update

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,44 +1,41 @@
-import React, { useMemo, memo } from 'react'
+import React, { useMemo } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import { pickComponent } from '../helpers'
 
 import { ItemProps } from '../interfaces'
 
-export const Item = memo<ItemProps & { readonly children: React.ReactNode }>(
-  ({ color, itemSize, shadow, children, style, Component, ...props }) => {
-    const Touchable = useMemo(() => pickComponent(Component), [])
+export const Item = ({
+  color,
+  itemSize,
+  shadow,
+  children,
+  style,
+  Component,
+  ...props
+}: ItemProps & { readonly children: React.ReactNode }) => {
+  const Touchable = useMemo(() => pickComponent(Component), [])
 
-    return (
-      <View
-        style={{
-          width: itemSize,
-          height: itemSize,
-          // padding: itemSize
-        }}
+  return (
+    <View style={{ width: itemSize, height: itemSize }}>
+      <Touchable
+        {...props}
+        style={StyleSheet.flatten([
+          style,
+          itemStyles({ color, itemSize }),
+          shadow && shadowStyles({ color }),
+        ])}
       >
-        <Touchable
-          {...props}
-          style={StyleSheet.flatten([
-            style,
-            itemStyles({ color, itemSize }),
-            shadow && shadowStyles({ color }),
-          ])}
-        >
-          {children}
-        </Touchable>
-      </View>
-    )
-  }
-)
+        {children}
+      </Touchable>
+    </View>
+  )
+}
 
 const itemStyles = StyleSheet.create(
   ({ color, itemSize }: Pick<ItemProps, 'color' | 'itemSize'>) => ({
     backgroundColor: color,
     borderRadius: itemSize / 2,
-    // margin: itemSize / 4,
-    // width: itemSize,
-    // height: itemSize,
     width: '100%',
     height: '100%',
   })
--- src/components/Marker/index.tsx
+++ src/components/Marker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, memo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { View, Animated, StyleSheet } from 'react-native'
 import Icon from 'native-icons'
 import Color from 'color'
@@ -10,110 +10,108 @@ import { MarkerProps } from '../../interfaces'
 
 import { checkColor } from '../../helpers'
 
-export const Marker = memo<MarkerProps>(
-  ({
-    color,
-    size,
-    animate,
-    markerDisplay = 'contrast',
-    markerType = 'border',
-    iconName = 'md-checkmark',
-    iconType = 'ionicons',
-    style,
-    ...props
-  }) => {
-    const scaleValue = new Animated.Value(0)
-    const rotateValue = new Animated.Value(1)
-    const fadeValue = new Animated.Value(0)
+export const Marker = ({
+  color,
+  size,
+  animate,
+  markerDisplay = 'contrast',
+  markerType = 'border',
+  iconName = 'md-checkmark',
+  iconType = 'ionicons',
+  style,
+  ...props
+}: MarkerProps) => {
+  const scaleValue = useMemo(() => new Animated.Value(0), [])
+  const rotateValue = useMemo(() => new Animated.Value(1), [])
+  const fadeValue = useMemo(() => new Animated.Value(0), [])
 
-    const [colorValue, setColorValue] = useState(color)
+  const [colorValue, setColorValue] = useState(color)
 
-    useEffect(() => {
-      let accent: string
+  useEffect(() => {
+    let accent: string
 
-      if (markerDisplay === 'adjust') {
-        accent = checkColor(color)
-      } else if (markerDisplay === 'contrast') {
-        accent = Color(color).isDark() ? '#fff' : '#000'
-      } else {
-        accent = markerDisplay
-      }
+    if (markerDisplay === 'adjust') {
+      accent = checkColor(color)
+    } else if (markerDisplay === 'contrast') {
+      accent = Color(color).isDark() ? '#fff' : '#000'
+    } else {
+      accent = markerDisplay
+    }
 
-      setColorValue(accent)
-    }, [])
+    setColorValue(accent)
+  }, [color, markerDisplay])
 
-    const opacity = useCallback(() => {
-      Animated.timing(scaleValue, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }).start()
+  const opacity = useMemo(() => {
+    Animated.timing(scaleValue, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start()
 
-      return animate && (markerType === 'icon' || markerType === 'border')
-        ? fadeValue
-        : 1
-    }, [])
+    return animate && (markerType === 'icon' || markerType === 'border')
+      ? fadeValue
+      : 1
+  }, [animate, fadeValue, markerType, scaleValue])
 
-    const scale = useCallback(() => {
-      Animated.timing(rotateValue, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true,
-      }).start()
+  const scale = useMemo(() => {
+    Animated.timing(rotateValue, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start()
 
-      return {
-        scale:
-          animate === 'scale' &&
-          (markerType === 'icon' || markerType === 'border')
-            ? scaleValue
-            : 1,
-      }
-    }, [])
+    return {
+      scale:
+        animate === 'scale' &&
+        (markerType === 'icon' || markerType === 'border')
+          ? scaleValue
+          : 1,
+    }
+  }, [animate, markerType, rotateValue, scaleValue])
 
-    const rotate = useCallback(() => {
-      Animated.timing(fadeValue, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }).start()
+  const rotate = useMemo(() => {
+    Animated.timing(fadeValue, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start()
 
-      return {
-        rotate:
-          animate === 'rotate' && markerType === 'icon'
-            ? rotateValue.interpolate({
-                inputRange: [0, 1],
-                outputRange: ['0deg', '80deg'],
-              })
-            : '0deg',
-      }
-    }, [])
+    return {
+      rotate:
+        animate === 'rotate' && markerType === 'icon'
+          ? rotateValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: ['0deg', '80deg'],
+            })
+          : '0deg',
+    }
+  }, [animate, fadeValue, markerType, rotateValue])
 
-    return (
-      <View {...props} style={StyleSheet.flatten([style, styles.wrapper])}>
-        <Animated.View
-          style={{
-            opacity: opacity(),
-            transform: [scale(), rotate()],
-          }}
-        >
-          {markerType === 'border' ? (
-            <BorderMarker size={size} color={colorValue} />
-          ) : markerType === 'icon' ? (
-            <Icon
-              testId="icon-marker"
-              type={iconType}
-              name={iconName}
-              size={(size / 3) * 2}
-              color={colorValue}
-            />
-          ) : (
-            markerType === 'fade' && <FadeMarker size={size} />
-          )}
-        </Animated.View>
-      </View>
-    )
-  }
-)
+  return (
+    <View {...props} style={StyleSheet.flatten([style, styles.wrapper])}>
+      <Animated.View
+        style={{
+          opacity,
+          transform: [scale, rotate],
+        }}
+      >
+        {markerType === 'border' ? (
+          <BorderMarker size={size} color={colorValue} />
+        ) : markerType === 'icon' ? (
+          <Icon
+            testId="icon-marker"
+            type={iconType}
+            name={iconName}
+            size={(size / 3) * 2}
+            color={colorValue}
+          />
+        ) : (
+          markerType === 'fade' && <FadeMarker size={size} />
+        )}
+      </Animated.View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: {
